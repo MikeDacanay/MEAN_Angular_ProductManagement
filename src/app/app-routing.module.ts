1583215825e83cac7e350d8bfcd787cc73087919
Mike Dacanay <migd64@gmail.com>
2017-09-20 20:44:23 -0700

    ALI clone this now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {EditProductComponent} from './edit-product/edit-product.component';
+import {CreateComponent} from './create/create.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'products',  component: ProductListComponent },
+{ path: 'edit',  component: EditProductComponent },
+{ path: 'create',  component: CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    EditProductComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
